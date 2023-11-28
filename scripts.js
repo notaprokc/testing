00f1ca7519440bb7080545a1df309fa2e5880dbc
notaprokc <152277738+notaprokc@users.noreply.github.com>
2023-11-28 20:38:24 +0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// Fetch card data from JSON file
+fetch('cards.json')
+  .then(response => response.json())
+  .then(cards => {
+    const cardContainer = document.querySelector('.card-container');
+
+    // Generate card elements
+    cards.forEach(card => {
+      const cardElement = document.createElement('div');
+      cardElement.classList.add('card');
+      cardElement.innerHTML = `
+        <h2>${card.title}</h2>
+        <p>${card.description}</p>
+      `;
+      cardContainer.appendChild(cardElement);
+    });
+
+    // Activate first card
+    const firstCard = cardContainer.querySelector('.card');
+    firstCard.classList.add('active');
+  });
